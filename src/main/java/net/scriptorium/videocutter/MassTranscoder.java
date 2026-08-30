689c0ea9ebb34fe54a950ee6e967f4220419f2d0
Alexander Pokropivny <alexander.pokropivny@gridnine.com>
2026-08-30 17:40:17 +0300

    Sun Aug 30 05:40:17 PM MSK 2026

--- src/main/java/net/scriptorium/videocutter/MassTranscoder.java
+++ src/main/java/net/scriptorium/videocutter/MassTranscoder.java
@@ -5,6 +5,7 @@ import net.scriptorium.videocutter.job.execution.ClipJobPerformer;
 import net.scriptorium.videocutter.media.Analysis;
 import net.scriptorium.videocutter.media.ShallowMediaInfo;
 import net.scriptorium.videocutter.util.FileUtil;
+import org.apache.logging.log4j.LogManager;
 
 import java.io.IOException;
 import java.nio.file.FileVisitResult;
@@ -18,6 +19,11 @@ import java.nio.file.attribute.BasicFileAttributes;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 import static net.scriptorium.videocutter.job.JobUtil.next;
 
@@ -36,33 +42,59 @@ public class MassTranscoder {
 		} else {
 			processed = Collections.emptySet();
 		}
+		final ExecutorService executor = Executors.newSingleThreadExecutor(r -> new Thread(r, "vc-mass-transcode"));
 		final FileVisitor<Path> visitor = new SimpleFileVisitor<>() {
 
 			@Override
 			public FileVisitResult visitFile(final Path filePath, final BasicFileAttributes attrs) throws IOException {
+				FileVisitResult result = FileVisitResult.CONTINUE;
 				final String filePathStr = filePath.toString();
 				if (!attrs.isSymbolicLink() && !processed.contains(filePathStr) && isVideo(filePath)) {
+					final long srcFileSize = Files.size(filePath);
 					final ShallowMediaInfo info = Analysis.shallowMediaInfo(filePath);
 					final int finishMillis = info.getDurationMillis();
 					final String format = "MP4".equals(info.getFormat()) ? "HEVC" : info.getFormat();
-					final FrameSize size = new FrameSize(String.valueOf(info.getWidth()), String.valueOf(info.getHeight()));
-					final MediaStream video = info.getVideo();
-					final ClipJob job = new ClipJob(0, finishMillis, format, size, video, null);
-					final Path out = next(filePath, job.format(), 1);
-					if (ClipJobPerformer.perform(job, filePath, out)) {
-						final long srcFileSize = Files.size(filePath);
-						final long outFileSize = Files.size(out);
-						if (outFileSize < srcFileSize) {
-							Files.move(out, filePath, StandardCopyOption.REPLACE_EXISTING);
-						} else {
-							FileUtil.delete(out);
+					final Path out = next(filePath, format, 1);
+					try {
+						final FrameSize size = new FrameSize(String.valueOf(info.getWidth()), String.valueOf(info.getHeight()));
+						final MediaStream video = info.getVideo();
+						final ClipJob job = new ClipJob(0, finishMillis, format, size, video, null);
+						boolean transcoded = false;
+						{
+							final Callable<Boolean> task = () -> ClipJobPerformer.perform(job, filePath, out);
+							final Future<Boolean> future = executor.submit(task);
+							do {
+								Thread.sleep(100);
+								final long outFileSize = Files.size(out);
+								if (outFileSize >= srcFileSize) {
+									future.cancel(true);
+									break;
+								}
+							} while (!future.isDone());
+							try {
+								transcoded = future.get();
+							} catch (final InterruptedException e) {
+								//
+							} catch (final ExecutionException e) {
+								result = FileVisitResult.TERMINATE;
+							}
 						}
-						Files.write(log, (System.lineSeparator() + filePathStr).getBytes(), StandardOpenOption.APPEND);
-					} else {
+						if (transcoded) {
+							final long outFileSize = Files.size(out);
+							if (outFileSize < srcFileSize) {
+								Files.move(out, filePath, StandardCopyOption.REPLACE_EXISTING);
+							}
+							Files.write(log, (System.lineSeparator() +
+									filePathStr).getBytes(), StandardOpenOption.APPEND);
+						}
+					} catch (final Exception e) {
+						LogManager.getLogger(MassTranscoder.class).error("failed to transcode file " + filePathStr, e);
+						throw new UncheckedException(e);
+					} finally {
 						FileUtil.delete(out);
 					}
 				}
-				return FileVisitResult.CONTINUE;
+				return result;
 			}
 		};
 		Files.walkFileTree(path, visitor);
